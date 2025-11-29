ecebdf59fad6b2cced86d113ee127cf4de77a04b
Kane Vidzro <kanevidzro@gmail.com>
2025-11-29 12:23:27 +0000

    redis v1

--- apps/api/lib/redis.ts
+++ apps/api/lib/redis.ts
@@ -1,5 +1,10 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis({
+  host: "redis",
+  port: 6379,
+  db: 0, // default database
+  // password: process.env.REDIS_PASSWORD,
+});
 
 export { redis };
